6f01eb29afa344d01f92f441b2236654f508a0e6
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-09-18 18:32:10 -0400

    Time: 50 ms (89.59%), Space: 19.1 MB (5.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,4 +1,11 @@
 from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return Counter(nums).most_common()[-1][0]
+        return Counter(nums).most_common()[-1][0]  #approach 1
+        # approach 2 - dict with counts?
+        # approach 3 - 2 sets 
+        # complete_set = set(nums)
+        # seen_set = set()
+        # for n in nums:
+        #     if n in seen_set:
+
