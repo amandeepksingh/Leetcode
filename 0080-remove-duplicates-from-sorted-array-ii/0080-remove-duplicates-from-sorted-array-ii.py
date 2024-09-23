5a02f621e5591ebe5c3809b3337196313ac03b87
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-09-23 12:10:36 -0400

    Time: 60 ms (12.1%), Space: 16.6 MB (50.24%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,16 +4,9 @@ class Solution:
         freq_dict = dict()
         for i in range(len(nums)): 
             if freq_dict.get(nums[i], 0) < 2:
-                print("at", nums[i])
-                print("k", k)
                 nums[k] = nums[i]
                 k = k + 1
                 freq_dict[nums[i]] = freq_dict.get(nums[i], 0) + 1
-                print(freq_dict[nums[i]])
-            elif freq_dict.get(nums[i], 0) >= 2:
-                print("else")
-                
-            print(nums)
         return k
 
             
