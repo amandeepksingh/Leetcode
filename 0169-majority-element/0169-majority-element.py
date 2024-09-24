831d8ba3a2e796467d294a5c8ff19c5ca6a40d56
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-09-24 10:38:42 -0400

    Time: 172 ms (50.37%), Space: 18.1 MB (22.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,10 +2,13 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         freq_dict = {}
+        # approach 1
         # for num in nums:
         #     freq_dict[num] = freq_dict.get(num, 0) + 1
         # for key, value in freq_dict.items():
         #     if value > len(nums)/2:
         #         return key
+
+        # approach 2
         return Counter(nums).most_common()[0][0]
         
