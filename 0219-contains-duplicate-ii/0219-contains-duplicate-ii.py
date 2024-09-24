9b28b9038399eb3bfc257d96cd1e8f39ffa6e391
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-09-24 14:50:41 -0400

    Time: 463 ms (51.13%), Space: 28.9 MB (75.5%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -3,7 +3,7 @@ class Solution:
         
         if len(nums) != len(set(nums)):
             for i in range(len(nums)):
-                for j in range(len(nums)):
+                for j in range(i, min(len(nums), i + k + 1)):
                     if i !=j and nums[i] == nums[j] and abs(i-j) <= k:
                         return True 
         return False
