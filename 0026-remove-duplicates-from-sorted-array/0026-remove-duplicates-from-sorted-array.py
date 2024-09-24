6c7b427e2e4f3dedf645f8defcaf5fe7b820fd84
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-09-24 14:35:53 -0400

    Time: 77 ms (47.07%), Space: 17.7 MB (95.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,12 +3,14 @@ class Solution:
         # approach 1 
         k = 0
         seen = set()
+        if len(set(nums)) == len(nums):
+            return len(nums)
         for i in range(len(nums)):
             if nums[i] not in seen:
                 nums[k] = nums[i]
                 seen.add(nums[i])
                 k = k + 1
-        # approach 2 - use collections counter and remove anything with a count greater than 1
+        # approach 2 - use collections counter and remove anything with a count greater than
         return k
 
                 
