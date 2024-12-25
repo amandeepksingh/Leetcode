144f520995aed32abaf1346c85ec653bf0d58ab7
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-12-25 01:44:27 -0500

    Time: 7 ms (39.42%), Space: 19.3 MB (5.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        num_max = max(num_set)
-        if len(num_set) == num_max+1:
+        num_max = max(nums)
+        if len(nums) == num_max+1:
             return num_max + 1
+        num_set = set(nums)
         for i in range(num_max):
             if i not in num_set:
                 return i
