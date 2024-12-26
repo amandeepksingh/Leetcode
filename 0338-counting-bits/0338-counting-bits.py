4cc0496fd8d709f564d5888be96bd4156058a325
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-12-26 17:33:01 -0500

    Time: 127 ms (5.2%), Space: 18.5 MB (16.15%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,5 +4,9 @@ class Solution:
         c = 0
         for i in range(1, n + 1):
             for _ in range(32):
+                # bit shifting is the same as halfing 
+                # countBits(n) = countBits(2n)
+                # countBits(n) + 1 = countBits(2n+1)
+                # so get the number of bits in half and then add 1 if odd else l
                 arr[i] = arr[i >> 1] + (i&1)
         return arr
