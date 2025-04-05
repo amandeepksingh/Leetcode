dee78f52d2a7d4840d8c5e2994c3e11fec958651
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2025-04-05 17:04:39 -0400

    Time: 11 ms (18.5%), Space: 19.6 MB (43.82%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        print(nums)
+        # print(nums)
         if len(nums) > 1:
             for n in range(0, len(nums)-1, 2):
                 # print(nums[n], nums[n+1])
