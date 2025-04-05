8678d5a2c243968f743ead16960899249b844915
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2025-04-05 17:04:16 -0400

    Time: 11 ms (18.5%), Space: 19.9 MB (9.4%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,7 +4,7 @@ class Solution:
         print(nums)
         if len(nums) > 1:
             for n in range(0, len(nums)-1, 2):
-                print(nums[n], nums[n+1])
+                # print(nums[n], nums[n+1])
                 if nums[n] != nums[n+1]:
                     return nums[n]
             return nums[-1]
