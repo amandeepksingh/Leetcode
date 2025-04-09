6585bb6b076dde8555b2c67b14bad78d99feb8d9
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2025-04-08 21:11:55 -0400

    Time: 0 ms (100%), Space: 17.7 MB (59.26%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = [[1]]
+        for i in range(1, numRows):
+            if i == 1:
+                rows.append([1, 1])
+            else:
+                tosum = 2**i # this is what it should sum up to
+                toapp = [0]*(i+1) # placeholder array 
+                toapp[0], toapp[-1] = 1, 1
+                tosum = tosum - 2 
+                while tosum > 0:
+                    for j in range(1, 1 + len(toapp)//2):
+                        toapp[j] = rows[i-1][j] + rows[i-1][j-1]
+                        toapp[-j - 1] = rows[i-1][j] + rows[i-1][j-1]
+                        tosum = tosum - 2*toapp[j]
+                    rows.append(toapp)
+        # print(toapp)
+        return rows 
+                    
