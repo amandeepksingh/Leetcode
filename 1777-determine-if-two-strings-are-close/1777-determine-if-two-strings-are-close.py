35037a1554f116b6270701f2d0428c663fa9e270
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2025-04-16 18:03:13 -0400

    Time: 78 ms (91.64%), Space: 18.1 MB (75.68%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -5,8 +5,8 @@ class Solution:
         if len(word1) == len(word2) and set(word1) == set(word2): 
             c1 = Counter(word1)
             c2 = Counter(word2)
-            print(list(c1.values()))
-            print(list(c2.values()))
+            # print(list(c1.values()))
+            # print(list(c2.values()))
             if sorted(list(c1.values())) == sorted(list(c2.values())):
                 return True
         return False
